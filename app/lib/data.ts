514d91be349e704a8b2e15ed082708623398c712
Craig Cooper <ccooper@mandmdirect.com>
2025-12-31 14:39:20 +0000

    Progress to Ch7

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -32,18 +32,19 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    const data = await sql<LatestInvoiceRaw[]>`
+		// Fetch the last 5 invoices, sorted by date
+		const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
 
-    const latestInvoices = data.map((invoice) => ({
-      ...invoice,
-      amount: formatCurrency(invoice.amount),
-    }));
-    return latestInvoices;
+		const latestInvoices = data.map((invoice) => ({
+			...invoice,
+			amount: formatCurrency(invoice.amount),
+		}));
+		return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch the latest invoices.');
