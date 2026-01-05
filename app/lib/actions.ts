66c039169459d1b33f547c54e2babb15f8a65e22
Craig Cooper <ccooper@mandmdirect.com>
2026-01-05 09:18:20 +0000

    to end Ch12

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,23 +18,29 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-	
-    const { customerId, amount, status } = CreateInvoice.parse({
+	const { customerId, amount, status } = CreateInvoice.parse({
 		customerId: formData.get("customerId"),
 		amount: formData.get("amount"),
 		status: formData.get("status"),
-    });
-    
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split("T")[0];
+	});
 
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
-    
-    revalidatePath("/dashboard/invoices");
-    redirect("/dashboard/invoices");
+	const amountInCents = amount * 100;
+	const date = new Date().toISOString().split("T")[0];
+
+	try {
+        await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+	} catch (error) {
+		console.error(error);
+		return {
+			message: "Database Error: Failed to create invoice.",
+		};
+	}
+
+	revalidatePath("/dashboard/invoices");
+	redirect("/dashboard/invoices");
 }
 
 // Use Zod to update the expected types
@@ -47,19 +53,29 @@ export async function updateInvoice(id: string, formData: FormData) {
 		status: formData.get("status"),
 	});
 
-	const amountInCents = amount * 100;
-
-	await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
+    const amountInCents = amount * 100;
+    
+    try {
+        await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+        `;
+    } catch (error) {
+        console.error(error);
+        return {
+			message: "Database Error: Failed to create invoice.",
+		};
+    }
 
 	revalidatePath("/dashboard/invoices");
 	redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
+    throw new Error("Failed to Delete Invoice");
+
+    // unreachable
 	await sql`DELETE FROM invoices WHERE id = ${id}`;
 	revalidatePath("/dashboard/invoices");
 }
